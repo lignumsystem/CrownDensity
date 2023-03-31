23dae27418eb6d9c8a2c5fb2f6e5df03283a9f61
jariperttunen <jari.perttunen@luke.fi>
2023-03-31 15:30:11 +0300

    Doxygen comments.

--- globalvariables.cc
+++ globalvariables.cc
@@ -1,11 +1,14 @@
 ///\file globalvariables.cc
 ///\brief Global variables
 ///
+///\defgroup Gvars Global variables
 ///Global variables used in LignumForest initialized by GrowthLoop.
 ///Variables are dummy in the context of CrownDensity, needed for compilation.
+///@{
 double H_0_ini = 0.0;
 double H_var_ini = 0.0;
 bool bud_variation = false;
 int n_buds_ini_max = 0.0;
 int n_buds_ini_min = 0.0;
 double rel_bud = 0.0;
+///@}

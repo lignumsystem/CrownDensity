359e8927de4042739da797c8a913f4a8d661eea3
jariperttunen <jari.perttunen@icloud.com>
2024-09-26 15:50:52 +0300

    It is now possible to give several MetaFiles and Growth Mode Change Years in the command line.
    For example
    
        ./crowndens 60 '{MetaFile,MetaFile1,MetaFile2}.txt' .... -modeChange 20,40 ....
    
    The usage must be consistent and it is checked in the command line parsing.
    The number of MetaFiles must be the number of Growth Mode Change year files + 1.
    The first MetaFile is consumed in the very first tree initialization.

--- run-crowndens-basic-model.sh
+++ run-crowndens-basic-model.sh
@@ -1,8 +1,14 @@
 #NOTE: the dummy '-iter <YEARS>' and -metafile MetaFile.txt argument. HDF5 files are generated with GrowthLoop from LignumForest.
 #The GrowthLoop command line checks for -iter argument.
 #Otherwise `crowndens` has compatible command line with `lignum-forest`.
+#NOTE: Glob expressions can be uses for MetaFiles: crowndens 60 '{Metafile,Metafile1}.txt'
+#      matches MetaFile.txt and MetaFile1.txt and nothing else.
+#      Protect the Glob expression with hyphens ('). See example in the command line below.
+#NOTE: -modeChange accepts comma separated list of years, e.g. -modeChage 10,20,30
+#NOTE: Consistency check is made: Number of MetaFiles must be number of ModeChange years + 1
+#      (The first Metafile is consumed in the first initialization).
 #REMEMBER: Set -hdf5 <filename> !!!!!!!
-./crowndens 60 MetaFile.txt -metafile MetaFile.txt -iter 60  -voxelspace VoxelSpace.txt -voxelCalculation 5 -numParts 2 -hw 10 -kBorderConifer 0.11 -modeChange 20 -architectureChange 5 -aChangeStart 20 -Lmaxturn 80 -writeInterval 5 -increaseXi 15  -hdf5 fip1-2fgo1-x1ac5.h5
+./crowndens 60 '{MetaFile,MetaFile1}.txt' -metafile MetaFile.txt -iter 60  -voxelspace VoxelSpace.txt -voxelCalculation 5 -numParts 2 -hw 10 -kBorderConifer 0.11 -modeChange 20 -architectureChange 5 -aChangeStart 20 -Lmaxturn 80 -writeInterval 5 -increaseXi 15  -hdf5 fip1-2fgo1-x1ac5.h5
 #-modeChange 20 -architectureChange 5 -aChangeStart 20 -Lmaxturn 80
 #-Ebhreduction 0.95 -EBHFINAL 0.5 -EBHInput 2
 #Ajo147
